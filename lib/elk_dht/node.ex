b1c4e5e2f9c79ad56a3e6a0dfdd3971b9aafc49f
Leeroy Ding <i@leeroy.im>
2015-05-13 17:49:40 +0800

    Add some GenServer callbacks

--- lib/elk_dht/node.ex
+++ lib/elk_dht/node.ex
@@ -9,7 +9,8 @@ defmodule ElkDHT.Node do
   @doc """
   Starts the node.
   """
-  def start_link(conf = %{id: node_id, host: _host, port: _port}, opts \\ []) do
+  def start_link(conf = %{id: node_id, host: host, port: port}, opts \\ []) do
+    Logger.debug "Starting a new node #{Hexate.encode(node_id)}: #{host}:#{port}"
     GenServer.start_link __MODULE__, conf, opts ++ [name: node_id]
   end
 
@@ -67,6 +68,30 @@ defmodule ElkDHT.Node do
   # Callbacks
 
   def init(%{id: node_id, host: host, port: port}) do
-    %{id: node_id, host: host, port: port, trans: HashDict.new, access_time: :os.timestamp}
+    Logger.debug "Initializing node #{node_id}."
+    {:ok, %{id: node_id, host: to_char_list(host), port: port, trans: HashDict.new, access_time: :os.timestamp}}
+  end
+
+  def handle_call({:add_trans, name, info_hash}, _from, state = %{trans: trans}) do
+    trans_id = Utils.random_trans_id
+    trans = HashDict.put trans, trans_id, {name, info_hash, :os.timestamp}
+    {:reply, trans_id, %{state | trans: trans}}
+  end
+
+  def handle_cast({:del_trans, trans_id}, state = %{trans: trans}) do
+    trans = HashDict.delete trans, trans_id
+    {:noreply, %{state | trans: trans}}
+  end
+
+  def handle_cast({:update_access, state}) do
+    {:noreply, %{state | access_time: :os.timestamp}}
+  end
+
+  def handle_cast({:send_message, message, socket, trans_id}, state = {host: host, port: port}) do
+    encoded = message
+    |> Map.put "v", Utils.get_version
+    |> Map.put "t", trans_id
+    |> Bencodex.encode
+    :gen_udp.send socket, host, port, [encoded]
   end
 end
