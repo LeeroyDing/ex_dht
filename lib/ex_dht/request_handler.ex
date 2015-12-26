e9d144e45ef5841915178bb4f425308977f94b49
Leeroy Ding <i@leeroy.im>
2015-12-26 19:49:33 +0000

    Add first request handler code

--- lib/ex_dht/request_handler.ex
+++ lib/ex_dht/request_handler.ex
@@ -1,6 +1,11 @@
 defmodule ExDHT.RequestHandler do
   use GenServer
 
-  def start_link, do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  def start_link(), do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
+
+  def handle_cast({:ping, %{"y" => "r", "r" => %{"id" => node_id}} = message}, state), do
+    # TODO: implement
+    {:noreply, state}
+  end
   
 end
