3cd3beb56fc9194ecbd43fc946ad5fc0fc0d7feb
Leeroy Ding <i@leeroy.im>
2015-12-27 23:01:00 +0000

    Add find_node handler

--- lib/ex_dht/request_handler.ex
+++ lib/ex_dht/request_handler.ex
@@ -3,9 +3,14 @@ defmodule ExDHT.RequestHandler do
 
   def start_link(), do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
 
-  def handle_cast({:ping, %{"y" => "r", "r" => %{"id" => node_id}} = message}, state), do
+  def handle_cast({:ping, %{"y" => "r", "r" => %{"id" => node_id}} = message}, state) do
     # TODO: implement
     {:noreply, state}
   end
-  
+
+  def handle_cast({:find_node, %{"y" => "r", "r" => %{"id" => node_id, "nodes" => raw_nodes}} = message}, state) do
+    #TODO: implement
+    {:noreply, state}
+  end
+
 end
