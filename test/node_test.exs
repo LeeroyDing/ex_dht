6607328029c0db5917cd2ab8656a05b697021290
Leeroy Ding <i@leeroy.im>
2016-07-16 23:00:33 +0100

    Add tests for token

--- test/node_test.exs
+++ test/node_test.exs
@@ -5,6 +5,8 @@ defmodule ExDHTTest.Node do
 
   @trans_name "Trans"
   @trans_info_hash nil
+  @token_info_hash "token_info_hash"
+  @token_value "123"
 
   test "Can create node" do
     {:ok, _} = Node.start_link("localhost", 0, Utils.random_node_id)
@@ -14,5 +16,14 @@ defmodule ExDHTTest.Node do
     {:ok, node} = Node.start_link("localhost", 0, Utils.random_node_id)
     trans_id = Node.add_trans(node, @trans_name, @trans_info_hash)
     assert Node.delete_trans(node, trans_id) == :ok
+  end
+
+  test "Can add / get / remove token" do
+    {:ok, node} = Node.start_link("localhost", 0, Utils.random_node_id)
+    assert Node.get_token(node, @token_info_hash) == nil
+    assert Node.add_token(node, @token_info_hash, @token_value) == :ok
+    assert Node.get_token(node, @token_info_hash) == @token_value
+    assert Node.delete_token(node, @token_info_hash) == :ok
+    assert Node.get_token(node, @token_info_hash) == nil
   end
 end
